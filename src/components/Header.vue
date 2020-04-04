1580a8dfd84bad22b4374f754a8a59c4e5c172a1
rif2d <saya@rifqi.id>
2020-04-04 20:14:02 +0700

    Add header navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>
-    Header
-  </div>
+  <header>
+    <!-- Image and text -->
+    <b-navbar variant="dark" type="dark">
+      <b-container fluid="lg">
+        <b-navbar-brand href="/">
+          <img
+            src="@/assets/bdm_logo.png"
+            class="d-inline-block align-top"
+            alt="Black Desert Mobile Planner"
+          />
+        </b-navbar-brand>
+
+        <b-navbar-nav>
+          <b-nav-item href="#">About</b-nav-item>
+          <b-nav-item href="#">GitHub</b-nav-item>
+        </b-navbar-nav>
+      </b-container>
+    </b-navbar>
+  </header>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
 };
 </script>
+
+<style scoped>
+.navbar-brand img {
+  max-height: 52px;
+}
+</style>
