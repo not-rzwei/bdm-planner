99e4c8ac536128494f5bffcbe71d7fb7e96c49aa
rif2d <saya@rifqi.id>
2020-04-04 18:43:04 +0700

    Add vue bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app">
-    BDM Planner
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    Header
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
