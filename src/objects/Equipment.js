e27799f6786017393f9fb5b95f090bf20f13eadf
rif2d <saya@rifqi.id>
2020-04-05 07:25:32 +0700

    Add equipment object and its unit test

--- src/objects/Equipment.js
+++ src/objects/Equipment.js
@@ -1,3 +1,20 @@
-class Equipment {}
+const Equipment = {
+  id: 0,
+  name: "",
+  type: "",
+  grade: "",
+  stats: {
+    ap: 0,
+    dp: 0
+  },
+  enhancement: {
+    max: 40,
+    current: 0
+  }
+};
+
+Equipment.fullName = function() {
+  return `${this.name} ${this.type}`;
+};
 
 export default Equipment;
--- tests/unit/equipment.spec.js
+++ tests/unit/equipment.spec.js
@@ -2,7 +2,22 @@ import { expect } from "chai";
 import Equipment from "../../src/objects/Equipment";
 
 describe("Equipment.js", () => {
-  it("create equipment instance", () => {
-    expect(new Equipment()).to.an("object");
+  it("return an object", () => {
+    expect(Equipment).to.be.an("object");
+  });
+
+  it("type return armor", () => {
+    var armor = Object.create(Equipment);
+    armor.type = "armor";
+
+    expect(armor.type).to.equal("armor");
+  });
+
+  it("return equipment full name", () => {
+    var bhegArmor = Object.create(Equipment);
+    bhegArmor.name = "Bheg's";
+    bhegArmor.type = "Armor";
+
+    expect(bhegArmor.fullName()).to.equal("Bheg's Armor");
   });
 });
