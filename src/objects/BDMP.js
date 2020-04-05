1f8cbe5b6d6daf86f0494b54b9e1061456c43a9d
rif2d <saya@rifqi.id>
2020-04-05 10:55:52 +0700

    Add useEquip method

--- src/objects/BDMP.js
+++ src/objects/BDMP.js
@@ -1,7 +1,27 @@
 import EquipmentList from "./EquipmentList";
+import Equipment from "./Equipment";
 
 class BDMP {
   equipmentList = EquipmentList;
+  equipment = {
+    mainhand: new Equipment({ type: "mainhand" }),
+    offhand: new Equipment({ type: "helmet" }),
+    helmet: new Equipment({ type: "helmet" }),
+    armor: new Equipment({ type: "armor" }),
+    gloves: new Equipment({ type: "gloves" }),
+    shoes: new Equipment({ type: "shoes" })
+  };
+
+  useEquip(id) {
+    var equip = this.equipmentList.find(eq => eq.id == id);
+
+    if (equip) {
+      this.equipment[equip.type] = equip;
+      return true;
+    }
+
+    return false;
+  }
 }
 
 export default BDMP;
--- tests/unit/bdmp.spec.js
+++ tests/unit/bdmp.spec.js
@@ -7,4 +7,21 @@ describe("BDMP.js", () => {
   it("return an array of equipment", () => {
     expect(new BDMP().equipmentList).to.be.an("array");
   });
+
+  it("return a mainhand", () => {
+    var mainhand = new BDMP().equipment.mainhand;
+    expect(mainhand)
+      .to.be.an("object")
+      .include({
+        type: "mainhand"
+      });
+  });
+
+  it("return noir glove", () => {
+    var bdmp = new BDMP();
+    var noir = bdmp.equipmentList.find(eq => eq.id == 501);
+    bdmp.useEquip(501);
+
+    expect(bdmp.equipment.gloves).to.equal(noir);
+  });
 });
