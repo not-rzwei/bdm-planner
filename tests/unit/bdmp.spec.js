1d08cbdbc04ff14cb774ec3a24baab2b4d371e89
rif2d <saya@rifqi.id>
2020-04-06 10:13:42 +0700

    Group equipment test

--- tests/unit/bdmp.spec.js
+++ tests/unit/bdmp.spec.js
@@ -2,35 +2,37 @@ import { expect } from "chai";
 import BDMP from "../../src/objects/BDMP";
 
 describe("BDMP.js", () => {
-  it("return an array of equipment", () => {
-    expect(new BDMP().equipmentList).to.be.an("array");
-  });
+  describe("Equipment test", () => {
+    it("return an array of equipment", () => {
+      expect(new BDMP().equipmentList).to.be.an("array");
+    });
 
-  it("return a mainhand", () => {
-    var mainhand = new BDMP().equipment.mainhand;
-    expect(mainhand)
-      .to.be.an("object")
-      .include({
-        type: "mainhand"
-      });
-  });
+    it("return a mainhand", () => {
+      var mainhand = new BDMP().equipment.mainhand;
+      expect(mainhand)
+        .to.be.an("object")
+        .include({
+          type: "mainhand"
+        });
+    });
 
-  it("return noir glove", () => {
-    var bdmp = new BDMP();
-    var noir = bdmp.equipmentList.find(eq => eq.id == 501);
-    bdmp.useEquip(501);
+    it("return noir glove", () => {
+      var bdmp = new BDMP();
+      var noir = bdmp.equipmentList.find(eq => eq.id == 501);
+      bdmp.useEquip(501);
 
-    expect(bdmp.equipment.gloves).to.equal(noir);
-  });
+      expect(bdmp.equipment.gloves).to.equal(noir);
+    });
 
-  it("return blank glove", () => {
-    var bdmp = new BDMP();
-    var noir = bdmp.equipmentList.find(eq => eq.id == 501);
+    it("return blank glove", () => {
+      var bdmp = new BDMP();
+      var noir = bdmp.equipmentList.find(eq => eq.id == 501);
 
-    bdmp.useEquip(501);
-    expect(bdmp.equipment.gloves).to.equal(noir);
+      bdmp.useEquip(501);
+      expect(bdmp.equipment.gloves).to.equal(noir);
 
-    bdmp.removeEquip(noir);
-    expect(bdmp.equipment.gloves).to.not.equal(noir);
+      bdmp.removeEquip(noir);
+      expect(bdmp.equipment.gloves).to.not.equal(noir);
+    });
   });
 });
